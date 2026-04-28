00f3cb28b2d7a1540d2e2df2e830defa9b9d72e4
suleman984 <sulemanefc@gmail.com>
2026-04-28 16:49:04 +0500

    babel issue resolved

--- mobile-app/metro.config.js
+++ mobile-app/metro.config.js
@@ -4,4 +4,10 @@ const config = getDefaultConfig(__dirname);
 
 config.resolver.unstable_enablePackageExports = true;
 
+// Force Babel transformation for packages that use import.meta (ESM-only syntax)
+// so babel-preset-expo's unstable_transformImportMeta plugin can rewrite them.
+config.transformer.transformIgnorePatterns = [
+  'node_modules/(?!(react-native|@react-native|expo|@expo|react-devtools-core|terser|sucrase|acorn|@babel/parser)/)',
+];
+
 module.exports = config;
